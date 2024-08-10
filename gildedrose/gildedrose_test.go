dba4dbf4b39cf879b43a28faa93f19fe1841fbb5
Mattsi <mattsi.jansky@codurance.com>
2024-08-10 19:59:42 +0100

    Add two basic tests for UpdateQuality

--- gildedrose/gildedrose_test.go
+++ gildedrose/gildedrose_test.go
@@ -18,3 +18,29 @@ func Test_Foo(t *testing.T) {
 		t.Errorf("Name: Expected %s but got %s ", expected, items[0].Name)
 	}
 }
+
+func TestShouldDegradeQualityByOne(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"foobar", 5, 5},
+	}
+
+	gildedrose.UpdateQuality(items)
+
+	const expected = 4
+	if items[0].Quality != expected {
+		t.Errorf("Quality: Expected %d but got %d ", expected, items[0].Quality)
+	}
+}
+
+func TestShouldReduceSellInByOne(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"foobar", 5, 5},
+	}
+
+	gildedrose.UpdateQuality(items)
+
+	const expected = 4
+	if items[0].SellIn != expected {
+		t.Errorf("SellIn: Expected %d but got %d ", expected, items[0].SellIn)
+	}
+}
